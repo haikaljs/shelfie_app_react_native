8bd879d81ebecc2747c5fb7b5d91567d87c89935
haikaljs <haikaljavascript@gmail.com>
2025-06-17 23:38:28 +0800

    File base navigation

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,19 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Linking } from "react-native";
 import Logo from "../assets/img/logo_light.png"
+import { Link } from 'expo-router'
+
 import React from "react";
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Image source={Logo} style={styles.img}/>
-      <Text style={[styles.title, {color: 'purple'}]}>The Number 1</Text>
+      <Text style={[styles.title]}>The Number 1</Text>
       <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
 
-      <View style={styles.card}>
-        <Text>Hello, this is Haikal</Text>
-      </View>
+      <Link href="/about" style={styles.link}>About page</Link>
+      <Link href="/contact" style={styles.link}>Contact page</Link>
+
     </View>
   );
 };
@@ -36,5 +38,9 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 5,
     borderShadow: '4px 4px rgba(0,0,0,0.1)'
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
   }
 });
