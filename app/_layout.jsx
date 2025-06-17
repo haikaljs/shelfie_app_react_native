ca04b3d5d3b77495b5c9a6e32ad4aa7835a9168b
haikaljs <haikaljavascript@gmail.com>
2025-06-18 00:45:29 +0800

    Light, dark theme

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,27 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
-import { name } from '../node_modules/ajv/dist/compile/codegen/code';
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import React from "react";
+import { Slot, Stack } from "expo-router";
+import { name } from "../node_modules/ajv/dist/compile/codegen/code";
+import { Colors } from "../constants/Colors";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
   return (
+    <>
+      <StatusBar style='auto'/>
       <Stack
         screenOptions={{
-          headerStyle: {backgroundColor: "#ddd"},
-          headerTintColor: '#333'
-        }
-        
-        }
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+        }}
       >
-        <Stack.Screen name="index" options={{title: 'Home'}}/>
-        <Stack.Screen name="about" options={{title: 'About'}}/>
-        <Stack.Screen name="contact" options={{title: 'Contact', headerShown: false}}/>
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="about" options={{ title: "About" }} />
+        <Stack.Screen
+          name="contact"
+          options={{ title: "Contact", headerShown: false }}
+        />
       </Stack>
-     
-   
-  )
-}
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
