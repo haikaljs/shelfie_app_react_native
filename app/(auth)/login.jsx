617e7ebff1fc1969ee16e5b55efd34bd824e4e00
haikaljs <haikaljavascript@gmail.com>
2025-06-20 23:11:06 +0800

    Pressable components

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,18 +1,29 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Pressable, Text } from "react-native";
 import { Link } from "expo-router";
+import { Colors } from "../../constants/Colors";
 
 // Themed components
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
+import ThemedButton from "../../components/ThemedButton";
 
 const Login = () => {
+
+  const handleSubmit = () => {
+    console.log('Login form submitted');
+  }
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
-        Reading List App
+        Login to your account
       </ThemedText>
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{color: '#f2f2f2'}}>Login</Text>
+      </ThemedButton>
 
       <Spacer height={100} />
    
@@ -30,11 +41,21 @@ export default Login;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     title: {
         textAlign: 'center',
         fontSize: 18,
         marginBottom: 30
+    },
+    btn: {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5
+    },
+    pressed: {
+        opacity: 0.8
     }
+
 });
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,12 +1,17 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { Link } from "expo-router";
 
 // Themed components
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
+import ThemedButton from "../../components/ThemedButton";
 
 const Register = () => {
+  const handleSubmit = () => {
+    console.log("Register form submitted");
+  };
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
@@ -14,13 +19,14 @@ const Register = () => {
         Register for an account
       </ThemedText>
 
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}>Register</Text>
+      </ThemedButton>
+
       <Spacer height={100} />
       <Link href="/login">
-        <ThemedText style={{ textAlign: "center" }}>
-          Login instead
-        </ThemedText>
+        <ThemedText style={{ textAlign: "center" }}>Login instead</ThemedText>
       </Link>
-    
     </ThemedView>
   );
 };
@@ -28,13 +34,14 @@ const Register = () => {
 export default Register;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center'
-    },
-    title: {
-        textAlign: 'center',
-        fontSize: 18,
-        marginBottom: 30
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center'
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 18,
+    marginBottom: 30,
+  },
 });
